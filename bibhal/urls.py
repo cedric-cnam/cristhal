813e1fdddf6076d2753a72b59d2af09057b80825
philippe <rigaux.phil@gmail.com>
2021-04-02 00:57:16 +0200

    Ajout menu publis

--- bibhal/urls.py
+++ bibhal/urls.py
@@ -5,6 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('accueil.urls')),
+    path('', include('publis.urls')),
      path('accounts/', include('django.contrib.auth.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
