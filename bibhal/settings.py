50e47ed38b28e36b9c6b39f6fbef4dea3a974a7a
philippe <rigaux.phil@gmail.com>
2021-04-02 00:42:27 +0200

    Modif settings

--- bibhal/settings.py
+++ bibhal/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accueil'
+    'accueil',
+    'publis'
 ]
 
 MIDDLEWARE = [
