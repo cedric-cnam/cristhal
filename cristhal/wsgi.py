4a3540a1248f87ad92342b452d23e2b3098d5533
philippe <rigaux.phil@gmail.com>
2021-04-05 10:27:31 +0200

    Renommage en cristhal

--- cristhal/wsgi.py
+++ cristhal/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bibhal project.
+WSGI config for crishal project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bibhal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crishal.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bibhal.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crishal.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
