644fefb1da98d324dbc93bda80b5fde61d68592d
philippe <rigaux.phil@gmail.com>
2021-04-08 12:18:29 +0200

    Changements doc

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -122,7 +122,8 @@ html_context = {
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "theme_footer_links"=False}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
