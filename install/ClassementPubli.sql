d09b2c7262d102ac77823cede4f7caac27317dc1
philippe <rigaux.phil@gmail.com>
2021-08-23 17:34:54 +0200

    Ajout des brevets et logiciels dans les publis à classer

---
+++ install/ClassementPubli.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO `ClassementPubli` (`code`, `libelle`) VALUES
+('C', 'Communications'),
+('I', 'Hors référentiel'),
+('N', 'National'),
+('N1', 'Q1'),
+('N2', 'Q2'),
+('N3', 'Q3'),
+('N4', 'Q4'),
+('V', 'Valorisation');
--- publis/views.py
+++ publis/views.py
@@ -256,8 +256,14 @@ def classement(request, code_collection):
                         ) |  Publication.objects.filter(
                         annee__gte=config.annee_min_publis).filter(
                         collections__code=code_collection).filter(type=PUBLI_REVUE)
+                        |  Publication.objects.filter(
+                        annee__gte=config.annee_min_publis).filter(
+                        collections__code=code_collection).filter(type=PUBLI_BREVET)    
+                        |  Publication.objects.filter(
+                        annee__gte=config.annee_min_publis).filter(
+                        collections__code=code_collection).filter(type=PUBLI_LOGICIEL)        
+                        
     context["classement_formset"] = PublisFormset (queryset=publis)
-
     context["matches"] = []
     context["bestrefs"] = []
     context["publis"] = publis
